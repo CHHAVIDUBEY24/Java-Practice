30dface6a02730fe1c410dfc90e7eb74084220d4
Chhavi Dubey <chhavidubey2224@gmail.com>
2026-07-08 08:38:32 +0000

    ArrayBasics updates

--- Core-java/ArrayBasics.java
+++ Core-java/ArrayBasics.java
@@ -1,6 +1,21 @@
 public class ArrayBasics {
     public static void main(String[] args) {
+        //arrays are mutable//
        int[] arr={1,2,3,4,5};
-        System.out.println(arr[0]);
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+        int[] arr1=new int[5];//5 size ka array banaya//
+        arr1[0]=1;
+        arr1[1]=2;
+        arr1[2]=3;
+        arr1[3]=4;
+        arr1[4]=5;
+        
+        System.out.println(arr1[0]);
+        
     }
 }
