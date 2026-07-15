5d4a31ca0c12a3648ea43b6b8a9ad44536295cbb
Chhavi Dubey <chhavidubey2224@gmail.com>
2026-07-15 16:11:49 +0000

    update the Scanner class

--- Core-java/ScannerBasic.java
+++ Core-java/ScannerBasic.java
@@ -6,5 +6,11 @@ public class ScannerBasic{
         System.out.println("Enter a number");
         double num=sc.nextDouble();
         System.out.println("You entered:"+num);
+
+        System.out.println("Enter your name");
+        String name=sc.nextLine();
+        System.out.println("Your entered name:"+name);
+        //nextLine() method is used to read the whole line of input including spces until the next line//
+        //next() is used to read the first word of the input//
     }
 }
