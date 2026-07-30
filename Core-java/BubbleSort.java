3703f5343fc6d38579d77b9bfd109b5fdadd96fa
Chhavi Dubey <chhavidubey2224@gmail.com>
2026-07-30 07:32:56 +0000

    Added bool variable

--- Core-java/BubbleSort.java
+++ Core-java/BubbleSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class BubbleSort{
     public static void main(String[] args)
     {
-        int arr[]={5,6,0,1,2,3,4};
+        int arr[]={5,6,0,1,2,3,4,9,0,3,4,5,6,6};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -11,6 +11,7 @@ public class BubbleSort{
         int n=arr.length;
         for(int i=0;i<n-1;i++)
         {
+            boolean swapped=false;
             for(int j=1;j<n-i;j++)
             {
                 if(arr[j-1]>arr[j])
@@ -18,9 +19,13 @@ public class BubbleSort{
                     int temp=arr[j-1];
                     arr[j-1]=arr[j];
                     arr[j]=temp;
+                    swapped=true;
                 }
             }
-
+            if(!swapped)
+            {
+                break;
+            }
         }
         
     }
