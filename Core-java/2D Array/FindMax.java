fa7ee4b65e8d03fcbb28e0daa13d2c4fcab94841
Chhavi Dubey <chhavidubey2224@gmail.com>
2026-08-11 04:47:31 +0000

    minval

--- Core-java/2D Array/FindMax.java
+++ Core-java/2D Array/FindMax.java
@@ -5,7 +5,7 @@ public class FindMax
     public static void main(String[] args) {
         int arr[][]=new int[3][4];
         Scanner sc=new Scanner(System.in);
-        int maxEle=Integer.MAX_VALUE;
+        int maxEle=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++)
         {
             for(int j=0;j<arr.length;j++)
@@ -13,9 +13,10 @@ public class FindMax
                 arr[i][j]=sc.nextInt();
             }
         }
+        System.out.println("\nYour Entered Matrix:");
         for(int i=0;i<arr.length;i++)
         {
-            for(int j=0;j<arr.length;j++)
+            for(int j=0;j< arr[0].length ; j++)
             {
                 System.out.print(arr[i][j]+" ");
             }
@@ -25,7 +26,7 @@ public class FindMax
         {
             for(int j=0;j<arr[0].length;j++)
             {
-                if(maxEle<arr[i][j])
+                if(maxEle < arr[i][j])
                 {
                     maxEle=arr[i][j];
                 }
